df57891c06c1ff61732dde5a10c100aa8b177e03
Daria Lepskaya <57892732+lepskaya@users.noreply.github.com>
2022-01-22 23:21:00 +0300

    Update lab1.cpp
    Добавлен вывод общей информации о задании

--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -1,8 +1,11 @@
 ﻿#include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello World!\n";
-	return 0;
+    cout << "Laboratory work #1. GIT\n";
+    cout << "Variant #6. Wind Rose\n";
+    cout << "Author: Lepskaya Darya\n";
+    return 0;
 }
-
